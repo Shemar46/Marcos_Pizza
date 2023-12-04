1209e41f7b0988bd5af97d1c2159c9f942ffd326
shema <shema@SHEMAR99>
2023-12-03 22:14:01 -0500

    Add project files.

---
+++ Marcos_Pizza/Data/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Marcos_Pizza.Data
+{
+    public class User : IdentityUser
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? gender { get; set; }
+
+        
+
+
+    }
+}
