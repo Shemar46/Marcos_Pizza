5d8b4f75d83f6a28f720273c99c5f94f88718f4c
Shemar Robinson <shemarrobinson403@gmail.com>
2023-12-07 22:18:28 -0500

    update before pull

---
+++ Marcos_Pizza/Configuration/MapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Marcos_Pizza.Data;
+using Marcos_Pizza.Models;
+
+namespace Marcos_Pizza.Configuration
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig() 
+        {
+            CreateMap<Order, OrderVM>().ReverseMap();
+            CreateMap<ProductC, ProductVM>().ReverseMap();
+            CreateMap<UserVM, UserVM>().ReverseMap();
+        }
+    }
+}
